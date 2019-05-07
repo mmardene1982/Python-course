1330ee92465c280e290dfafd8bd160133b674de8
mmardene1982 <mmardene1982@gmail.com>
2019-05-07 15:06:38 +0200

    added in the pc

---
+++ getUserneame.py
@@ -0,0 +1,22 @@
+import  json
+import socket
+import getpass
+from urllib.request import urlopen
+username= getpass.getuser()
+print ('USERNAME     :   ', username)
+
+hostname= socket.gethostname()
+print('HOSTNAME     :   ', hostname)
+
+machineIP= socket.gethostbyname(hostname)
+print('IP ADDRESS   :   ', machineIP)
+
+url="http://ipinfo.io/json"
+response= urlopen(url)
+
+data= json.load(response)
+
+print('PUBLIC IP    :   ', data['ip'])
+print('CITY         :   ', data['city'])
+print('REGION       :   ', data['region'])
+print('COUNTRY      :   ', data['country'])
