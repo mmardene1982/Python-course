849f3c4de2fab858f4398df499177bf37ca67fc5
mmardene1982 <mmardene1982@gmail.com>
2020-02-12 11:57:45 +0100

    added in the pc

--- test progress.py
+++ test progress.py
@@ -1,8 +1,21 @@
-from tqdm import tnrange
-import random
-import time
-numberList=[1,2,3,4,5,6,7,8,9]
-for i in range(15):
-   print(random.choice(numberList))
-   time.sleep(1)
 
+import fbchat
+from fbchat import Client
+from getpass import getpass
+
+import random, time
+
+username= "maher.mardini1"
+client= fbchat.Client(username, getpass())
+name= str(input("name: "))
+friends= client.searchForUsers(name)
+friend=friends[0]
+msg=["\U0001f600","\U0001F606","\U0001F923",]
+for i in range(15):
+    sent=client.send(fbchat.models.Message(random.choice(msg)),friend.uid)
+    time.sleep(1)
+
+
+if sent:
+    print("all good")
+    client.logout()
